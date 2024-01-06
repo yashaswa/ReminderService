f0746d927debe0d8d89647ace36b9cf7e8b0edf7
yashaswa <sharmayashswa777@gmail.com>
2024-01-06 21:38:22 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const  { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = ()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}))
 
     app.listen( PORT  ,()=>{
-        console.log(`Server started at port ${PORT}`)
+        console.log(`Server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'sharmayashaswa777@gmail.com',
+            'this is a texting email',
+            'Hey ! how are you do you like the support gmail service'
+        );
     });
 }
 setupAndStartServer();
---
+++ src/services/email-service.js
@@ -0,0 +1,23 @@
+const sender = require('../config/emailConfig');
+
+const sendBasicEmail = async (mailFrom ,mailTo, mailSubject , mailBody) =>{
+
+
+    try {
+        const response = await sender.sendMail({
+            from : mailFrom,
+            to : mailTo,
+            subject: mailSubject,
+            text : mailBody
+        })
+    } catch (error) {
+        console.log(error);
+        
+        
+    }
+  
+}
+
+module.exports = {
+    sendBasicEmail
+}
